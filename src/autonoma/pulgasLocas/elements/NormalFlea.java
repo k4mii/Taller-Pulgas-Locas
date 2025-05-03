3caf93e51bb98da078f8ceb23086bb717bcc99f6
Camila <mariac.pradac@autonoma.edu.co>
2025-05-03 15:47:14 -0500

    Se agrego la imagen en la clase pulga normal

--- src/autonoma/pulgasLocas/elements/NormalFlea.java
+++ src/autonoma/pulgasLocas/elements/NormalFlea.java
@@ -1,18 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package autonoma.pulgasLocas.elements;
 
 import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.ImageIcon;
 
 /**
  *
  * @author marti
  */
 public class NormalFlea extends Flea {
+    private Image normalFleaImage;
     public NormalFlea(int x, int y, int height, int width,int step) {
         super(x, y, height, width,step);
+        normalFleaImage = new ImageIcon(getClass().getResource("/autonoma/pulgasLocas/images/PulgaNormal.png")).getImage();
+       
     }
 
     
@@ -30,6 +31,6 @@ public class NormalFlea extends Flea {
 
     @Override
     public void paint(Graphics g) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        g.drawImage(normalFleaImage, x, y, null);
     }
 }
