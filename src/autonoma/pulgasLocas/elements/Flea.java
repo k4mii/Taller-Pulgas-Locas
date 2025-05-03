6c1ad78f393a7ba8a36f39cfb31c672bfe6fe099
Martin <martintabordaaristizabal@gmail.com>
2025-05-03 12:43:58 -0500

    SE agrega la clase pulga con toda su informacion

---
+++ src/autonoma/pulgasLocas/elements/Flea.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package autonoma.pulgasLocas.elements;
+
+import gamebase.elements.SpriteMobile;
+
+/**
+ *
+ * @author marti
+ */
+public abstract class Flea extends SpriteMobile {
+    
+    public Flea(int x, int y, int width, int height, int step) {
+        super(x, y, height, width);
+    }
+
+    // Método que debe implementar cada tipo de pulga (normal o mutante)
+    public abstract void detach(Battlefield battlefield);
+}
---
+++ src/autonoma/pulgasLocas/elements/MutantFlea.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package autonoma.pulgasLocas.elements;
+
+import java.awt.Graphics;
+
+/**
+ *
+ * @author marti
+ */
+public class MutantFlea extends Flea {
+
+    
+}
